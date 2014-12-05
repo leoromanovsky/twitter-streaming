ce1135179d5808f1b13e4b2885c7bdd1b11c2fa3
Leo Romanovsky <leoromanovsky@gmail.com>
2014-12-04 23:14:43 -0800

    Produce top 10 retweeted elements in a sliding window.

--- src/main/scala/MedisasJob.scala
+++ src/main/scala/MedisasJob.scala
@@ -3,6 +3,27 @@ import StreamingContext._
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.twitter._
 import org.apache.spark.SparkConf
+import org.apache.spark.Logging
+import org.apache.log4j.{Level, Logger}
+
+/*
+Medisas Instructions:
+Use Twitter's sample streaming API to show the top 10 retweeted tweets
+(note the retweeted_status field) in a rolling window of time,
+where the window's start is n minutes ago (where n is defined by the user)
+and the window's end is the current time.
+
+Output should continuously update and include the tweet text and
+number of times retweeted in the current rolling window.
+You should not use the retweet_count field,
+but instead count the retweets that your program actually processes.
+
+How to run, for example:
+MedisasJob 60 60
+
+This accumulates 60 seconds worth of data and runs the job every 60 seconds.
+While not exact, there should not be any overlap in this case.
+ */
 
 object MedisasJob {
   def main(args: Array[String]) {
@@ -11,6 +32,8 @@ object MedisasJob {
       System.exit(1)
     }
 
+    // Configuration.
+    Logger.getRootLogger.setLevel(Level.ERROR)
     System.setProperty("twitter4j.oauth.consumerKey", "8AqQCy7umStCyNN356v7fw")
     System.setProperty("twitter4j.oauth.consumerSecret", "vOvKV1QwuS1AeKPMIvJqErBxW7i1N12OL4UY2tNMs0c")
     System.setProperty("twitter4j.oauth.accessToken", "29463499-9Og6hxW4HqFxcQyIrAdmLpbAnrwIk290ghOE0ez5f")
@@ -21,25 +44,42 @@ object MedisasJob {
     val sparkConf = new SparkConf()
       .setMaster("local[2]")
       .setAppName("TwitterPopularTags")
-    // Run the job every 10 seconds.
+    // Run the job every `runInterval` seconds.
     val ssc = new StreamingContext(sparkConf, Seconds(runInterval.toInt))
     val stream = TwitterUtils.createStream(ssc, None, Seq())
 
-    val hashTags = stream
-      .flatMap(status => status.getText.split(" ")
-      .filter(_.startsWith("#")))
+    /*
+      1. Filter out Tweets that are not retweets. This checks whether `retweeted_status` is not null.
 
-    // Accumulate 60 seconds worth of tweets.
-    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(windowLength.toInt))
-      .map{ case (topic, count) => (count, topic)}
+      2. Map: Having verified that `retweeted_status` is present for each element in the RDD,
+        the key is made to be the parent tweet (that which is being retweeted) and value is scalar 1.
+        This is a common approaching for performing map-reduce counts.
+
+      3. reduceByKeyAndWindow:
+        By reducing with the parent tweet as the key, this operation produces a resultant list such as:
+        ( (tweet, count), (tweet, count), ... )
+
+      4. Map: Reverse the members of the tuple. This prepares it for sorting.
+
+      5. Sort: Sort by key, which is now the count. False parameter signifies descending order.
+     */
+    val tweets = stream
+      .filter { case(tweet) => tweet.isRetweet() }
+      .map { case(tweet) => (tweet.getRetweetedStatus(), 1) }
+      .reduceByKeyAndWindow(_ + _, Seconds(windowLength.toInt))
+      .map{ case (tweet, count) => (count, tweet)}
       .transform(_.sortByKey(false))
 
-    topCounts10.foreachRDD(rdd => {
+    // Take the top 10 elements in the RDD and produce human readable results.
+    tweets.foreachRDD(rdd => {
       val topList = rdd.take(10)
-      println("Top 10")
+      println("\n\n\n\n\n\n## Start of Window ##")
       topList.foreach {
-        case(count, tag) => println("%s (%s tweets)".format(tag, count))
+        case(count, tweet) =>
+          println("(ID: %s, Retweets in window: %s)".format(tweet.getId, count))
+          println("\t%s\n".format(tweet.getText().split('\n').map(_.trim.filter(_ >= ' ')).mkString))
       }
+      println("** End of Window **")
     })
 
     ssc.start()
